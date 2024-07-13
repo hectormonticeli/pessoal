a76c3e1f27e134456fcdb7afe8c96b73a541ec74
Hector Monticeli Arantes <hectormonticelia@gmail.com>
2024-07-13 17:57:31 -0300

    concluí o rodapé e o header a princípio

--- html-css/projetos/login-signin/javascript/script.js
+++ html-css/projetos/login-signin/javascript/script.js
@@ -1,9 +1,16 @@
 function clickMenuLogin() {
     openButton = document.getElementById('open');
     closeButton = document.getElementById('close');
+    menu = document.getElementById('login-signup');
 
     if (openButton.style.display == 'block') {
         openButton.style.display = 'none';
         closeButton.style.display = 'block';
+        menu.style.display = 'block';
+    }
+    else {
+        openButton.style.display = 'block';
+        closeButton.style.display = 'none';
+        menu.style.display = 'none';
     }
 }
