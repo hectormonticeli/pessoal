b75a02e407f03d7903b6ea8fda5c016c8ac5f81d
Hector Monticeli Arantes <hectormonticelia@gmail.com>
2024-07-17 16:01:45 -0300

    botao ativado

--- html-css/projetos/login-signin/javascript/script.js
+++ html-css/projetos/login-signin/javascript/script.js
@@ -1,7 +1,12 @@
+mizunoClick = 0;
+clicked = 0;
+symbol = 0;
+
 function clickMenuLogin() {
     openButton = document.getElementById('open');
     closeButton = document.getElementById('close');
     menu = document.getElementById('login-signup');
+    main = document.getElementById('main');
 
     if (openButton.style.display == 'block') {
         openButton.style.display = 'none';
@@ -14,3 +19,27 @@ function clickMenuLogin() {
         menu.style.display = 'none';
     }
 }
+
+/* function filterByBrand() {
+    mizunoCards = document.getElementsByClassName('mizuno');
+    nikeCards = document.getElementsByClassName('nike');
+    adidasCards = document.getElementsByClassName('adidas');
+    umbroCards = document.getElementsByClassName('umbro');
+    
+    mizunoCards.style.display = 'none';
+} */
+
+function checked() {
+    mizunoClick += 1;
+
+    if(mizunoClick % 2 == 1) {
+        mizuno.style.background = '#BB342F';
+        mizuno.style.color = 'white';
+    }
+    else if (clicked % 2 == 0) {
+        mizuno.style.background = 'transparent';
+        mizuno.style.color = '#D90429';
+    }
+}
+
+/*  onmouseover="this.style.background = '#BB342F'" onmouseout="if(mizunoClick % 2 == 0){this.style.background = 'transparent'}" */
